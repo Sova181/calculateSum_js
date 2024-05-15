d6f99f7ca0de58720db5ee21f9f2e3cd35def50d
Таня Дашипилова <tanya.volk01@mail.ru>
2024-05-15 21:43:16 +0800

    export +

--- func_calc.js
+++ func_calc.js
@@ -19,11 +19,13 @@ function calculateSum(array) {
     return sum;
   }
 
+module.exports = calculateSum
+
+  /*
   const coll1 = [8, 9, 21, 19, 18, 22, 7];
   console.log(calculateSum(coll1)); // Выведет: 48
-  
   const coll2 = [2, 0, 17, 3, 9, 15, 4];
   console.log(calculateSum(coll2)); // Выведет: 27
-  
   const coll = [];
   console.log(calculateSum(coll)); // Выведет: 0
+  */
---
+++ main.js
@@ -0,0 +1,9 @@
+const func = require('./func_calc');
+
+const coll1 = [8, 9, 21, 19, 18, 22, 7]; //  48
+const coll2 = [2, 0, 17, 3, 9, 15, 4]; //  27
+const coll = []; // 0
+
+console.log(func(coll1)); // Выведет: 48
+console.log(func(coll2)); // Выведет: 27
+console.log(func(coll)); // Выведет: 0
